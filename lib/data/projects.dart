6574b506bb5d4569f68b5af7edd43c149fab1f7a
FG Dev Studio <ferdiangunawan98@gmail.com>
2021-10-11 02:08:45 +0700

    git

--- lib/data/projects.dart
+++ lib/data/projects.dart
@@ -12,8 +12,7 @@ class Project {
 List<Project> PROJECTS = [
   Project(
     name: 'Ontix',
-    description:
-        'Developing a Ticketing booking application with Flutter SDK and Firebase',
+    description: 'Developing a Ticketing booking application with Flutter SDK and Firebase',
     image: 'images/projects/ontix.jpeg',
     url: 'https://play.google.com/store/apps/details?id=com.fgdev.ontix',
     skills: [
@@ -21,15 +20,15 @@ List<Project> PROJECTS = [
       'Json',
       'Firebase',
       'Flutter',
-      'Git',
+      'NodeJs'
+          'Git',
     ],
   ),
   Project(
     name: 'Food delivery app',
     description: 'Developing food delivery app with beautiful interface',
     image: 'images/projects/foodApp.png',
-    url:
-        'https://drive.google.com/file/d/1jIq4THe3MykC3mP5_ju8ye_grWVXVLVx/view?usp=sharing',
+    url: 'https://drive.google.com/file/d/1jIq4THe3MykC3mP5_ju8ye_grWVXVLVx/view?usp=sharing',
     skills: [
       'Dart',
       'Flutter',
@@ -39,11 +38,9 @@ List<Project> PROJECTS = [
   ),
   Project(
     name: 'Service Guidance',
-    description:
-        'Learning apps for mechanic student to do service on heavy equipment',
+    description: 'Learning apps for mechanic student to do service on heavy equipment',
     image: 'images/projects/serviceguidance.jpeg',
-    url:
-        'https://drive.google.com/file/d/1vSifFw948s9qiP2SMF9bLIJ_RvKkpOBA/view?usp=sharing',
+    url: 'https://drive.google.com/file/d/1vSifFw948s9qiP2SMF9bLIJ_RvKkpOBA/view?usp=sharing',
     skills: [
       'Java',
       'Android Studio',
@@ -53,11 +50,9 @@ List<Project> PROJECTS = [
   ),
   Project(
     name: 'Machine Health and Condition Monitoring',
-    description:
-        'Monitoring realtime data Komatsu PC3000-6E Electric by Android (IoT)',
+    description: 'Monitoring realtime data Komatsu PC3000-6E Electric by Android (IoT)',
     image: 'images/projects/mhcm.jpeg',
-    url:
-        'https://drive.google.com/file/d/1DlSViRbXZk9YQW7g3xjBYBaLunTxLs4o/view?usp=sharing',
+    url: 'https://drive.google.com/file/d/1DlSViRbXZk9YQW7g3xjBYBaLunTxLs4o/view?usp=sharing',
     skills: [
       'Java',
       'Android Studio',
@@ -69,8 +64,7 @@ List<Project> PROJECTS = [
     name: 'Cozy Room',
     description: 'Find cozy house to stay and happy',
     image: 'images/projects/kos.png',
-    url:
-        'https://drive.google.com/file/d/13K0vHobP-VmXmwDoMlT633GLul-wj94n/view?usp=sharing',
+    url: 'https://drive.google.com/file/d/13K0vHobP-VmXmwDoMlT633GLul-wj94n/view?usp=sharing',
     skills: [
       'Dart',
       'Flutter',
